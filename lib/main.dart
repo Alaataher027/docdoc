151fe38fbffb12944589a2b5b283741fc42175e6
Alaataher027 <alaataher027@gmail.com>
2025-11-28 21:01:12 +0200

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,7 @@
+import 'package:advanced_flutter_project/core/routes/app_router.dart';
+import 'package:advanced_flutter_project/doc_app.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(DocApp(appRouter: AppRouter()));
+}
